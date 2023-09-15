f93d9120bc880a1b11babf94aad751b08bd62a01
7R1XY <theo.broden@gmail.com>
2023-09-15 12:47:00 +0200

    Add project files.

---
+++ Uppgift 2.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Uppgift_2._2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt mätt i meter hoppade du Elin?");
+            string Elinshopp = Console.ReadLine();   
+            double längdtext1 = double.Parse(Elinshopp);
+            Console.WriteLine("Hur långt mätt i meter hoppade du Alva?");
+            string Alvashopp = Console.ReadLine();
+            double längtext2 = double.Parse(Alvashopp); 
+            double Skillnad = längdtext1 - längtext2;
+            Console.WriteLine("Elin hoppade " +  Skillnad + " meter längre än Alva"); 
+            
+
+        }
+    }
+}
